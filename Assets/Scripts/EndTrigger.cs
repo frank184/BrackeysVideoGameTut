197945a727ee25e3a5b5082f1bc74657d654cbba
frank184 <francoisbelanger1993@hotmail.com>
2018-06-30 04:37:28 -0400

    init

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EndTrigger : MonoBehaviour {
+
+    public GameManager manager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        manager.CompleteLevel();
+    }
+}
---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class HUD : MonoBehaviour {
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("QUIT");
+    }
+}
